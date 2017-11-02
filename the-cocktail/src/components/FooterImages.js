d309f53f5daab364153e90f626546187ef7fb597
Jimena <jime.ebermudez@gmail.com>
2017-11-02 02:30:10 +0100

    Cambio en logos. Cambios en el footer para que las imágenes no se sobrepongan en tamaño xs

--- the-cocktail/src/components/FooterImages.js
+++ the-cocktail/src/components/FooterImages.js
@@ -30,26 +30,26 @@ class FooterImages extends Component {
                 <Col lg="4" className="d-none d-lg-block logo--decoration"></Col>
                 <Col xs="12" lg="8" className="logo--container">
                     <Row>
-                        <Col xs="3" className="align-self-center">
+                        <Col xs="6" lg="3" className="align-self-center">
                             <img id="Popover1" className="logo--img" src={img1} alt="Non-vessel operating common carrier" onClick={() => this.toggle(0)}/>
                             <Popover placement="top" isOpen={this.state.popoverOpen[0]} target="Popover1" toggle={this.toggle}>
 
                                 <PopoverBody className="popover-top">Non-vessel operating common carrier</PopoverBody>
                             </Popover>
                         </Col>
-                        <Col xs="3" className="align-self-center">
+                        <Col xs="6" lg="3" className="align-self-center">
                             <img id="Popover2" className="logo--img" src={img2} alt="The United States Federal Maritime Commission" onClick={() => this.toggle(1)}/>
                             <Popover placement="top" isOpen={this.state.popoverOpen[1]} target="Popover2" toggle={this.toggle}>
                                 <PopoverBody>The United States Federal Maritime Commission</PopoverBody>
                             </Popover>
                         </Col>
-                        <Col xs="3" className="align-self-center">
+                        <Col xs="6" lg="3" className="align-self-center">
                             <img id="Popover3" className="logo--img" src={img3} alt="Member of the World Cargo alliance" onClick={() => this.toggle(2)}/>
                             <Popover placement="top" isOpen={this.state.popoverOpen[2]} target="Popover3" toggle={this.toggle}>
                                 <PopoverBody>Member of the World Cargo alliance (WCA)</PopoverBody>
                             </Popover>
                         </Col>
-                        <Col xs="3" className="align-self-center">
+                        <Col xs="6" lg="3" className="align-self-center">
                             <img id="Popover4" className="logo--img" src={img4} alt="Federación Española de Transitarios Expedidores Internacionales y Asimilados" onClick={() => this.toggle(3)}/>
                             <Popover placement="top" isOpen={this.state.popoverOpen[3]} target="Popover4" toggle={this.toggle}>
                                 <PopoverBody>Federación Española de Transitarios Expedidores Internacionales y Asimilados</PopoverBody>
